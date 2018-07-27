34421f00c312846fe89ad8afa96cffb199424f1d
Ranjit Shinde <ranjitshinde@in-ranjitbs.local>
2018-07-27 12:23:59 +0530

    working react app

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require("webpack")
+var path = require("path")
+
+var DIS_DIR = path.resolve(__dirname, "dist")
+var SRC_DIR = path.resolve(__dirname, "src")
+
+var config = {
+  entry: SRC_DIR+"/app/index.js",
+  output:{
+    path:DIS_DIR+"/app",
+    filename:"bundle.js",
+    publicPath:"/app/"
+  },
+  module:{
+    rules:[{
+      test:/\.js?/,
+      include:SRC_DIR,
+      loader:"babel-loader",
+      options:{
+        presets:["react", "es2015", "stage-2"]
+      }
+  }]
+  }
+};
+
+module.exports = config;
